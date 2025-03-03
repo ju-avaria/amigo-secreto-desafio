bd4c34608eed7b6f887abfa384bed612df5f3b25
ju-avaria <juanavariag@gmail.com>
2025-03-02 21:30:00 -0300

    Actualizacion app.js

--- app.js
+++ app.js
@@ -5,21 +5,30 @@ function agregarAmigo(){
 	// Obtiene el valor del input
 	let amigo = document.getElementById("amigo").value;
 	// si el input esta vacio
-	if (amigo == ""){
+	if (!amigo){
 		alert('Por favor Ingresa un nombre.');
 
-	} else{
+	} 
+	// Si el input tiene algun valor
+	else{
 		// Se agrega el nombre a la lista
 		amigos.push(amigo);
-		console.log(amigo);
-		console.log(amigos);
+		// console.log(amigo);
+		// console.log(amigos);
+
 		// Vacia el input
 		vaciarInput();
+
+		// Agregar los elementos a la lista HTML
+		listarAmigosHTML(amigos);
+		
 
 		return;
 
 	}
 }
+
+
 // Esta funcion deja en blanco el input
 function vaciarInput() {
 	let valorInput = document.getElementById('amigo');
@@ -27,3 +36,34 @@ function vaciarInput() {
 	return vaciarInput;
 	
 }
+// Esta funcion muestra los nombres de los amigos en el HTML
+function listarAmigosHTML(amigos) {
+	let listaAmigos = document.getElementById('listaAmigos');
+	listaAmigos.innerHTML = "";
+
+	for (i = 0; i < amigos.length ; i++){
+		let nombreAmigo = document.createElement("li");
+		nombreAmigo.textContent = amigos[i];
+		listaAmigos.appendChild(nombreAmigo);
+
+	}
+	return;
+}
+
+// Se sortea un amigo al azar
+function sortearAmigo() {
+	if(amigos.length === 0){
+		alert("No Hay Amigos Para Sortear");
+	} 
+	else{
+		let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+		let resultadoSorteo = document.getElementById('resultado');
+		resultadoSorteo.innerHTML = `El amigo sorteado es : ${amigoSorteado}`;
+	}
+
+}
+
+
+
+
+
